52405e1b17de6bd36d896e3daf005293b595917a
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-03-24 17:46:29 +0200

    Trying to connect the FE with the BE but not working properly due to my lack of knowledge in JS

---
+++ src/main/java/config/CorsConfiguration.java
@@ -0,0 +1,24 @@
+package config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration extends org.springframework.web.cors.CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        WebMvcConfigurer.super.configurePathMatch(configurer);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
